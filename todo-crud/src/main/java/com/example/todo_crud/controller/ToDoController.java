2effafe640a80f5dfa3725ae5c608ffb43362df3
seulki-k <skbb11@naver.com>
2024-08-25 17:58:59 +0900

    a

---
+++ todo-crud/src/main/java/com/example/todo_crud/controller/ToDoController.java
@@ -0,0 +1,13 @@
+package com.example.todo_crud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ToDoController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+}
